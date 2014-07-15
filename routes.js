d29c2e446eae352c0229828cfab4bd026a5e1d3d
Brendon Gonzalez <brendon@brandingbrand.com>
2014-07-15 13:26:13 -0400

    Upgrade to express 4

---
+++ routes.js
@@ -0,0 +1,49 @@
+exports.registerRoutes = function(app) {
+  //Register controllers
+  var index = require('./controllers/index')(app);
+  var api = require('./controllers/api')(app);
+
+  //Register routes
+  app.route('/')
+    .get(index.index);
+
+  // API
+  // Clients
+  app.route('/api/clients')
+    .get(api.clients.index)
+    .post(api.clients.create);
+
+  app.route('/api/clients/:clientId')
+    .get(api.clients.show)
+    .put(api.clients.update)
+    .delete(api.clients.remove);
+
+  app.route('/api/clients/:clientId/projects')
+    .get(api.clients.projects);
+
+  // Projects
+  app.route('/api/projects')
+    .get(api.projects.index)
+    .post(api.projects.create);
+
+  app.route('/api/projects/:projectId')
+    .get(api.projects.show)
+    .put(api.projects.update)
+    .delete(api.projects.remove);
+
+  app.route('/api/projects/:projectId/locales')
+    .get(api.projects.locales);
+
+  app.route('/api/projects/:projectId/locales/:locale')
+    .get(api.projects.locale);
+
+  // Locales
+  app.route('/api/locales')
+    .get(api.locales.index)
+    .post(api.locales.create);
+
+  app.route('/api/locales/:localeId')
+    .get(api.locales.show)
+    .put(api.locales.update)
+    .delete(api.locales.remove);
+};
