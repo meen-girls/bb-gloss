e0916946f931d2b5f818f9ea5ca1d4a9113d9c70
NickMele <nickmele26@gmail.com>
2014-07-16 19:40:03 -0400

    new add functionality

--- models/locale.js
+++ models/locale.js
@@ -5,15 +5,15 @@ var schema = new Schema({
   locale: {
     type: String,
     required: true,
-    trim: true,
-    unique: true
+    trim: true
   },
   project: {
     type: Schema.Types.ObjectId,
     ref: 'project',
     required: true
   },
-  translations: Schema.Types.Mixed
+  translations: Schema.Types.Mixed,
+  isDefault: Boolean
 });
 
 // create the model
