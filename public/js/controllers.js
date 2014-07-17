9d7ba36f75bb7f7f444da2b945a1ea962804bf85
pixeldev <archangel43@gmail.com>
2014-07-16 20:32:32 -0400

    FrontEnd locale iterator

---
+++ public/js/controllers.js
@@ -0,0 +1,33 @@
+Translator.ProjectLocalesController = Ember.ArrayController.extend({
+  translatedKeys: function() {
+    var ar = this.get('content');
+    var tranName = '';
+    var newArray = [];
+    var obj = {};
+
+    for (var i = 0; i < ar.length; i++) {
+      var dataSplit = ar[i].key.split('.');
+      var newName = dataSplit[0];
+      if (tranName !== newName) {
+        //Only run this if it's gone through the program
+        if (tranName !== '') {
+          newArray.push(obj);
+        }
+
+        tranName = newName;
+        obj = {keys: []};
+        obj.name = tranName;
+      }
+
+      obj.keys.push(ar[i]);
+      // Have to do it for the last one in the array
+      if (i === ar.length-1) {
+        newArray.push(obj);
+      }
+    }
+
+    return newArray;
+  }.property('content')
+
+});
+
